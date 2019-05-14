dc76d26f9aa7bccc9033484a3f40cdd5548179fb
Ivan <idyptan@mapr.com>
2019-05-14 19:16:32 +0300

    merging onto static project

---
+++ web/src/main/java/com/dyptan/controller/StreamingController.java
@@ -0,0 +1,43 @@
+package com.dyptan.controller;
+
+import com.dyptan.model.Car;
+import com.dyptan.streaming.spark.ModelTrainer;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.io.IOException;
+
+
+@RestController
+public class StreamingController {
+
+    private final ReactiveMongoTemplate mongoTemplate;
+    private final ModelTrainer trainer = new ModelTrainer();
+
+    public StreamingController(ReactiveMongoTemplate mongoTemplate) throws IOException {
+        this.mongoTemplate = mongoTemplate;
+    }
+
+    @GetMapping(
+            value = {"/mongostream"},
+            produces = {"text/event-stream"}
+    )
+    public Flux<Car> olxCars() {
+        return this.mongoTemplate.tail(new Query(), Car.class).share();
+    }
+
+    @PostMapping("/trainModel")
+    public void train(){
+        trainer.train();
+    }
+
+    @PostMapping("/saveModel")
+    public void save(){
+        trainer.save();
+    }
+
+}
